5eacaf6061ecaee4a56baee11b1563b03ab7294e
Sharvil Kekre <sharvilkekre@gmail.com>
2020-09-27 20:19:26 -0700

    commit .

--- cmd/commit.go
+++ cmd/commit.go
@@ -19,7 +19,6 @@ import (
 	"fmt"
 	"os/exec"
 	"errors"
-	"time"
 	"github.com/spf13/cobra"
 )
 
@@ -66,9 +65,8 @@ Combines add, commit, and push into one command.`,
 		if err != nil {
 			fmt.Println(err)
 		}
-		for i:= 10; i>=0; i-- {
+		for i:= 1000000; i>=0; i-- {
 			fmt.Printf("\033[2K\r%d", i)
-			time.Sleep(1 * time.Second)
 		}
 		fmt.Println()
 	},
