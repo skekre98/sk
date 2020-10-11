ab3c680e6174262eb5458f3be0f3c2b6901f867d
Sharvil Kekre <sharvilkekre@gmail.com>
2020-10-10 20:23:22 -0700

    created release script

---
+++ release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+while getopts "v:" opt; do
+  case $opt in
+    v) version=$OPTARG   ;;
+    *) echo 'error' >&2
+       exit 1
+  esac
+done
+
+if [ -z "$version" ]
+then
+  echo "release version is empty...";
+fi
+
+
+function tag_and_release {
+	git tag -a v$version -m "Releasing new version" master
+    git push --tags
+}
+
+tag_and_release
